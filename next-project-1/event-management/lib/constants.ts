2015c921d06abc24de38d337a04d1a4a68adaa6a
Raees Khan <rk7523505@gmail.com>
2026-07-15 17:01:38 +0500

    created home page for Event-management

---
+++ next-project-1/event-management/lib/constants.ts
@@ -0,0 +1,34 @@
+export const events = [
+  {
+    title: "React Summit 2026",
+    slug: "react-summit-2026",
+    location: "Amsterdam, Netherlands",
+    date: "September 16-18, 2026",
+    time: "09:00",
+    image: "/images/event1.png",
+  },
+  {
+    title: "GitHub Universe",
+    slug: "github-universe-2026",
+    location: "San Francisco, USA",
+    date: "October 28-30, 2026",
+    time: "08:30",
+    image: "/images/event2.png",
+  },
+  {
+    title: "AI Builders Night",
+    slug: "ai-builders-night",
+    location: "London, UK",
+    date: "August 12, 2026",
+    time: "18:30",
+    image: "/images/event3.png",
+  },
+  {
+    title: "Hack the Future",
+    slug: "hack-the-future",
+    location: "Toronto, Canada",
+    date: "November 7-8, 2026",
+    time: "10:00",
+    image: "/images/event4.png",
+  },
+];
--- next-project-1/event-management/src/app/page.js
+++ next-project-1/event-management/src/app/page.js
@@ -1,4 +1,7 @@
 import React from "react";
+import ExploreBtn from "./components/ExploreBtn";
+import EventCard from "./components/EventCard";
+import { events } from "../../lib/constants";
 
 const page = () => {
   return (
@@ -9,6 +12,18 @@ const page = () => {
       <p className="text-center mt-5">
         Hackathons, Meetups, Conferences, All in One Place
       </p>
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events list-none">
+          {events.map((event) => (
+            <li key={event.title} className="">
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
